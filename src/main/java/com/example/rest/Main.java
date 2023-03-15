69516294d47d971a29166443b8f6af59bbe5b7cd
Anthony Angelo <anthonyangelo@yahoo.com>
2023-03-15 01:10:55 -0400

    Update

--- src/main/java/com/example/rest/Main.java
+++ src/main/java/com/example/rest/Main.java
@@ -12,8 +12,12 @@ import java.net.URI;
  *
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+    // Listen from localhost only
+    // public static final String BASE_URI = "http://localhost:8080/myapp/";
+    
+    // Listen from all hosts
+    public static final String BASE_URI = "http://localhost:0.0.0.0/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
